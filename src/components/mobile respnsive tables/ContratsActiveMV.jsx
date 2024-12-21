02202edbe3d61f5c4de679fecae9677cb3aa1b86
YOUCEF54 <94319560+YOUCEF54@users.noreply.github.com>
2024-12-21 16:05:45 +0100

    Fixes

--- src/components/mobile respnsive tables/ContratsActiveMV.jsx
+++ src/components/mobile respnsive tables/ContratsActiveMV.jsx
@@ -3,11 +3,10 @@ import MobileTableCard from "../cards/MobileTableCard";
 // eslint-disable-next-line react/prop-types
 export default function ContratsActiveMV({className}) {
   return (
-    <div className={`flex flex-col p-6  ${className}`}>
+    <div className={`flex flex-col p-6 whitespace-nowrap  ${className}`}>
     <div className="flex sticky left-0 top-0 bg-white filter-none backdrop-blur-lg bg-opacity-50 pb-6  right-0  items-center justify-between">
       <h1 className="font-semibold text-xl">Contarats Active MV</h1>
       <div className="right-0">
-        {/* <DropDown /> */}
       </div>
     </div>
     <div className=" flex flex-col gap-6 ">
