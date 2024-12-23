ab8510cd4a5bd43111775f00ad51ffb73265655b
YOUCEF54 <94319560+YOUCEF54@users.noreply.github.com>
2024-12-23 09:40:47 +0100

    Updates

--- src/components/cards/TopVehiculesCard.jsx
+++ src/components/cards/TopVehiculesCard.jsx
@@ -1,37 +1,31 @@
-import { UserCircleIcon } from '@heroicons/react/24/solid'
 
 // eslint-disable-next-line react/prop-types
-export default function BestClients({ className }) {
+export default function TopVehicules({ className }) {
   return (
     <div className={`flex flex-col p-6 min-h-60 ${className}`}>
       <div className="flex sticky left-0 top-0 bg-white filter-none backdrop-blur-lg bg-opacity-50 pb-6  right-0  items-center justify-between">
-        <h1 className="font-semibold text-xl">Meilleurs clients</h1>
+        <h1 className="font-semibold text-xl">Meilleurs véhicules</h1>
         <div className="right-0">
         </div>
       </div>
-      <div className="overflow-x-auto overflow-y-clip">
-        <table className="w-full min-w-[50rem] border-collapse ">
+      <div className="overflow-x-auto overflow-y-clip ">
+        <table className="w-full  min-w-[50rem] border-collapse ">
           <thead>
             <tr className="bg-slate-200 bg-opacity-70">
-              <th className="rounded-l-lg font-medium">Full Name</th>
-              <th className="p-3 font-medium">Telephone</th>
-              <th className="p-3 font-medium">Identification</th>
-              <th className="p-3 font-medium">N permis</th>
-              <th className="p-3 font-medium">Amount</th>
+              <th className="rounded-l-lg font-medium">Véhicule</th>
+              <th className="p-3 font-medium">Nombre de contrats</th>
+              <th className="p-3 font-medium">Nombre de jours loué</th>
+              <th className="p-3 font-medium">Montant</th>
               <th className="p-3 font-medium rounded-r-lg">Status</th>
             </tr>
           </thead>
           <tbody className="divide-y-2">
             {Array.from({ length: 3 }).map((_, index) => (
               <tr key={index} className="text-neutral-600">
-                <td className="p-6 flex justify-center gap-4 items-center">
-                  <UserCircleIcon className="size-9" />
-                  Salama Aazzat
-                </td>
-                <td className="p-6 text-center">+440800000</td>
-                <td className="p-6 text-center">SH101010</td>
-                <td className="p-6 text-center">423</td>
-                <td className="p-6 text-center">1000DH</td>
+                <td className="p-6 text-center">Clio 4</td>
+                <td className="p-6 text-center">8</td>
+                <td className="p-6 text-center">7</td>
+                <td className="p-6 text-center">500DH</td>
                 <td className="p-4 text-center">
                   {index !== 2 ? (
                     <span className="bg-emerald-500 font-medium text-white py-1 px-6 rounded-full">Active</span>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -11,6 +11,7 @@ import RevenueChart from "../components/charts/RevenueChart";
 import ContratsActiveMV from "../components/mobile respnsive tables/ContratsActiveMV";
 import ReservationsMobile from "../components/mobile respnsive tables/ReservationsMobile";
 import GantChart from "../components/charts/GantChart";
+import TopVehicules from "../components/cards/TopVehiculesCard";
 
 export default function Dashboard() {
 
@@ -149,14 +150,15 @@ export default function Dashboard() {
             <Reservations />
           </div>
           <div className="grid grid-cols-12 lg:gap-8 max-lg:flex max-lg:flex-col">
-            <ContratsActive className="rounded-2xl max-sm:hidden col-span-8 my-4 max-h-[30rem] shadow-md overflow-hidden bg-white w-full"/>
-            <ContratsActiveMV className="rounded-2xl max-h-[18rem]  sm:hidden  my-4  overflow-hidden  w-full"/>
+            <ContratsActive className="rounded-2xl max-sm:hiddenn col-span-8 my-4 max-h-[30rem] shadow-md overflow-hidden bg-white w-full"/>
+            {/* <ContratsActiveMV className="rounded-2xl max-h-[18rem]  sm:hidden  my-4  overflow-hidden  w-full"/> */}
             <ReservationsCard className="rounded-2xl whitespace-nowrap col-span-4 my-4 max-h-[30rem]d sm:p-6 sm:bg-white  overflow-hidden  w-full"/>
             
           </div>
           <RevenueChart className="bg-white  shadow-md rounded-2xl p-4 pb-0 mb-8 mt-4" />
           <GantChart/>
           <BestClients className="rounded-2xl my-4 max-h-[30rem] shadow-md overflow-hidden bg-white w-full" />
+          <TopVehicules className="rounded-2xl my-4 max-h-[30rem] shadow-md overflow-hidden bg-white w-full" />
         </main>
 
       </div>
