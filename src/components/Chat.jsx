aa935338df508eb98e21dade89d35eedaeee727b
YOUCEF54 <94319560+YOUCEF54@users.noreply.github.com>
2024-12-24 22:02:42 +0100

    updates

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import { ChatBubbleBottomCenterIcon, ChatBubbleLeftRightIcon, ChatBubbleOvalLeftIcon, Cog8ToothIcon, CogIcon, MinusIcon, PlusIcon, UserCircleIcon } from "@heroicons/react/16/solid";
+import { ChatBubbleLeftRightIcon,CogIcon, MinusIcon, PaperClipIcon, PlusIcon, UserCircleIcon } from "@heroicons/react/16/solid";
 import { useState } from "react";
 
 export default function Chat() {
@@ -6,16 +6,16 @@ export default function Chat() {
 
   return (
    <div>
-    <div onClick={()=>{setisOpen(!isOpen)}} className={`${isOpen ? " max-w-[90vw] w-[25rem] h-[16rem] bg-white rounded-lg shadow-md border" : "size-16 bg-orange-200 p-3 bg-opacity-75 backdrop-blur-lg hover:bg-orange-100 duration-75 cursor-pointer border border-orange-400  rounded-full"} z-50 duration-100  absolute right-4 bottom-4`}>
-        <ChatBubbleLeftRightIcon className={`${isOpen?"hidden":"hover:scale-105 fill-orange-400"} max-w-16 duration-100 `}/>
+    <div onClick={()=>{setisOpen(!isOpen)}} className={`${isOpen ? "flex flex-col max-w-[90vw] w-[25rem] mi-h-[20rem]  bg-white rounded-lg shadow-md border" : "size-16 bg-orange-200 p-3 bg-opacity-75 backdrop-blur-lg hover:bg-orange-100 duration-75 cursor-pointer border border-orange-400  rounded-full"} z-50 duration-200 ease-in-out  absolute right-4 bottom-4 overflow-y-hidden`}>
+        <ChatBubbleLeftRightIcon className={`${isOpen?"hidden":" hover:scale-105 fill-orange-400"}  duration-100 `}/>
         {isOpen ?
         <div onClick={(e)=>{e.stopPropagation()}}>
             <div className="flex items-center justify-between gap-2 p-2 py-4 border-b" >
-                <div className="flex items-center gap-2 " >
+                <div className="flex cursor-pointer items-center gap-2 " >
                     <UserCircleIcon className="size-8"/>
                     <div>Youcef El Omari</div>
                 </div>
-                <div className="flex items-center gap-2">
+                <div onClick={()=>{setisOpen(false)}} className="flex cursor-pointer items-center gap-2">
                     <CogIcon className="size-6 bg-neutral-100 border border-neutral-300 rounded-full p-[2px] "/>
                     <MinusIcon className="size-6"/>
                     <PlusIcon className=" rotate-45 size-6"/>
@@ -23,11 +23,22 @@ export default function Chat() {
 
 
             </div>
-            <div>
+            <div className="flex flex-col flex-grow h-full gap-2 min-h-[20rem] overflow-y-auto">
                 <div className="flex items-center p-2 px-3 gap-2">
                     <UserCircleIcon className="size-6"/>
                     <div className=" p-2 bg-orange-100 rounded-lg px- relative before:absolute before:text-[12px] whitespace-nowrap before:-bottom-5 before:content-['22_Dec_2024,_1:31_PM']">Hello! How can I assist you?</div>
                 </div>
+                <div className="flex items-center p-2 px-3 gap-2  right-0 w-full  justify-end  relative before:absolute before:text-[12px] whitespace-nowrap  before:-bottom-3 before:content-['22_Dec_2024,_1:31_PM']">
+                    <div className=" p-2 bg-neutral-100 rounded-lg px- ">Okey</div>
+                </div>
+                <div className="flex items-center p-2 px-3 gap-2">
+                    <UserCircleIcon className="size-6"/>
+                    <div className=" p-2 bg-orange-100 rounded-lg px- relative before:absolute before:text-[12px] whitespace-nowrap before:-bottom-5 before:content-['22_Dec_2024,_1:31_PM']">Thank you?</div>
+                </div>
+            </div>
+            <div className=" w-full h-[4rem] relative flex items-center  ">
+                <input placeholder="What can we help you with?" className="outline-none relative w-full px-4 pr-16  p-2 bg-neutral-50 h-full bottom-0"/>
+                <PaperClipIcon className="absolute size-9 text-neutral-800 cursor-pointer hover:bg-neutral-100 rounded-md  right-4 p-1.5"/>
             </div>
         </div>
         : null}
